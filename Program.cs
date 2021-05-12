ff31b2a65545984db59df3e070f5e185aa9e2873
Paweł Łęczycki <pawel@msd.info.pl>
2021-05-12 08:50:06 +0200

    Init

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom._12_14._05._2021.WPCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
