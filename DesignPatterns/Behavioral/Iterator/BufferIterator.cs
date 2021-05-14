35bf66d72ed27bb77bfe9aeb9c068de01f2c9203
Paweł Łęczycki <pawel@msd.info.pl>
2021-05-14 09:36:12 +0200

    Iterator

---
+++ DesignPatterns/Behavioral/Iterator/BufferIterator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Altkom._12_14._05._2021.WPCSharp.DesignPatterns.Behavioral.Iterator
+{
+    class BufferIterator<T> : IEnumerator<Tuple<T, T>>
+    {
+        private IEnumerator<T> enumerator;
+
+        public BufferIterator(IEnumerable<T> enumerable)
+        {
+            enumerator = enumerable.GetEnumerator();
+            Reset();
+        }
+
+        public Tuple<T, T> Current { get; private set; }
+
+        object IEnumerator.Current => Current;
+
+        public void Dispose()
+        {
+            enumerator.Dispose();
+            enumerator = null;
+            Current = null;
+        }
+
+        public bool MoveNext()
+        {
+            var buffer = enumerator.Current;
+            if (enumerator.MoveNext())
+            {
+                Current = new Tuple<T, T>(buffer, enumerator.Current);
+                return true;
+            }
+            return false;
+        }
+
+        public void Reset()
+        {
+            enumerator.Reset();
+            enumerator.MoveNext();
+            Current = null;
+        }
+    }
+}
---
+++ DesignPatterns/Behavioral/Iterator/Client.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom._12_14._05._2021.WPCSharp.DesignPatterns.Behavioral.Iterator
+{
+    public class Client
+    {
+        public static void Execute()
+        {
+            var list = new List<string> { "a", "b", "c", "d", "e" };
+
+            var buffer = new Buffer<string>(list);
+
+            var list2 = new List<string>();
+            foreach (var item in buffer)
+            {
+                var x = item.Item1 + item.Item2;
+                Console.WriteLine(x);
+                list2.Add(x);
+            }
+
+            foreach (var item in new Buffer<string>(list2))
+            {
+                var x = item.Item1 + item.Item2;
+                Console.WriteLine(x);
+            }
+
+        }
+    }
+}
